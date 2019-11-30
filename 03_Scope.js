a563e633eb5cfc5c7b9f87fc8529f34574dd62b3
Stanislav <sheveliukhin.s@gmail.com>
2019-11-30 16:38:30 +0600

    Strange and easy scope lesson

---
+++ 03_Scope.js
@@ -0,0 +1,26 @@
+// global scope, local scope
+// document, window objects of browser have global scope. Available everywhere
+
+function funcA()
+{
+    let a = 1;
+    function funcB()
+    {
+        let b = 2;
+        function funcC()
+        {
+            let c = 3;
+            console.log('func C: ', a, b, c);
+        }
+
+        funcC();
+        console.log('func B: ', a, b);
+    }
+
+    funcB();
+    console.log('func A: ', a);
+}
+
+funcA();
+
+//////////////////////////////////////////////////
