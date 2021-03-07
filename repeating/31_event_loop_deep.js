c98a118b1d542e83993847ec5ef5d8346a1364cb
lightxlamp <sheveliukhin.s@gmail.com>
2021-03-08 00:43:55 +0600

    small async test

--- repeating/31_event_loop_deep.js
+++ repeating/31_event_loop_deep.js
@@ -1,3 +1,19 @@
 // https://www.youtube.com/watch?v=8aGhZQkoFbQ&t
 
-// http://latentflip.com/loupe/ - 
+// http://latentflip.com/loupe/ 
+
+setTimeout(function timeout() {
+    console.log('Hey');
+}, 1000)
+
+setTimeout(function timeout() {
+    console.log('Hey');
+}, 1000)
+
+setTimeout(function timeout() {
+    console.log('Hey');
+}, 1000)
+
+setTimeout(function timeout() {
+    console.log('Hey');
+}, 1000)
