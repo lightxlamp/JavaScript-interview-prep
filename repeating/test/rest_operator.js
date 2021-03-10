106656c7dee63547b2fb3414604de337dc98c150
lightxlamp <sheveliukhin.s@gmail.com>
2021-03-11 01:58:09 +0600

    rest documentation test

--- repeating/test/rest_operator.js
+++ repeating/test/rest_operator.js
@@ -11,3 +11,20 @@ function hey(a, b, ...others) {
 }
 
 hey('Stas', 'Tilek', 'Maria', 'Kairat', 'JSON', 'Igor', 'Alex')
+
+// A function definition's last parameter can be prefixed with "..."
+// Hmmm...
+// Let's rewrite to test =) smth 
+
+console.log('**********************************');
+
+function heyV2(a, ...others, b) { // Error - SyntaxError: Rest parameter must be last formal parameter. Nice
+    console.log(a);
+    console.log(b);
+    console.log(others);
+    others.forEach(other => {
+        console.log(other);
+    })
+}
+
+heyV2('Stas', 'Tilek', 'Maria', 'Kairat', 'JSON', 'Igor', 'Alex')
