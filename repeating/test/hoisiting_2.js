463615dbf49b86c716685ff3b5e59f0cf8103e15
lightxlamp <sheveliukhin.s@gmail.com>
2021-03-11 01:49:12 +0600

    class declarations - hoisting

--- repeating/test/hoisiting_2.js
+++ repeating/test/hoisiting_2.js
@@ -4,9 +4,25 @@ function x(n) {
     return n * n;
 }
 
-y();
+// y(); // error - Cannot access 'y' before initialization
 
 const y = function () {
     console.log('Y');
 }
+
+// let bmw = new Car('E34', 2001) // error - Cannot access 'Car' before initialization
+// class declarations - hoisting
+
+class Car  {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
+    }
+}
+
+let myCar = new Car('Prius', 2006)
+console.dir(myCar)
+
+
+
 
