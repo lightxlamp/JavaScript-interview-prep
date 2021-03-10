2a7238a41640fe9e883d4cb9be646634662a81e9
lightxlamp <sheveliukhin.s@gmail.com>
2021-03-11 01:43:29 +0600

    tests

---
+++ repeating/test/hoisiting.js
@@ -0,0 +1,7 @@
+console.log(i);
+console.log(j);
+console.log(x);
+
+var i = 5;
+let j = 5;
+let x = 5;
---
+++ repeating/test/hoisiting_2.js
@@ -0,0 +1,12 @@
+console.log(x(12));
+
+function x(n) {
+    return n * n;
+}
+
+y();
+
+const y = function () {
+    console.log('Y');
+}
+
